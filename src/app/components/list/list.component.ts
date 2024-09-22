97473b858a883be3962a0b3e894fb8ba4bf93a14
EvertonWallace <everton542@hotmail.com>
2024-09-22 19:27:59 -0300

    refatoracao listagem de produto criando um component para redenrizacao do mesmo

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,14 +1,14 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../interfaces/product.interface';
-import {MatCardModule} from "@angular/material/card";
-import {MatButtonModule} from "@angular/material/button";
+import { CardComponent } from './card/card.component';
+
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
