6117ece1ee0892eb60cec93a7a705ebc97440e18
EvertonWallace <everton542@hotmail.com>
2024-09-22 20:06:26 -0300

    adicao de roteamento para o create component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './components/list/list.component';
+import { CreateComponent } from './components/create/create.component';
 
 export const routes: Routes = [
   {path:'',
     component: ListComponent
+  },
+  {
+    path:'create-product',
+    component:CreateComponent
   }
 ];
