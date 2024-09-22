8a9f0c7ff6b7b0c56d8e23dfc8ba5b2775fc038b
EvertonWallace <everton542@hotmail.com>
2024-09-22 19:54:30 -0300

    adicao de link para criar um produto e estilizacao deste link

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -2,13 +2,15 @@ import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product } from '../../interfaces/product.interface';
 import { CardComponent } from './card/card.component';
+import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, RouterLink, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.css'
 })
